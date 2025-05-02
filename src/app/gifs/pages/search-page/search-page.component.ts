bdceaa00a67ed23f0c04048c1deddca14122a955
Kenny Carmona <kennyharold02@gmail.com>
2025-05-02 15:48:16 -0500

    Search Page

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [ GifListComponent],
   templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+  onSearch(query: string) {
+    console.log(query);
+  }
+ }
